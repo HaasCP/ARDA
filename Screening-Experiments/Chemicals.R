a29dbbe4d22c824c98d5afcc0535d0e5c3ed3669
HaasCP <chrhaas07@gmail.com>
2019-10-30 12:52:31 +0100

    Initial commit

---
+++ Screening-Experiments/Chemicals.R
@@ -0,0 +1,45 @@
+## This R script provides all required information on the chemicals used in this project.
+##
+# Chemicals ---------------------------------------------------------------
+
+reactants <- list(
+  "NMQ" = list(
+    "CAS number" = "606-43-9",
+    "detectWavelength" = "328",
+    "calibFactor" = NULL,
+    "retTime" = 2.8,
+    "bottleConc" = 0.04, # mol/L
+    "channel" = "B"
+  ), 
+  "Coumarin" = list(
+    "CAS number" = "91-64-5",
+    "detectWavelength" = "280",
+    "calibFactor" = NULL,
+    "retTime" = 3.6,
+    "bottleConc" = 0.04, # mol/L
+    "channel" = "C"
+  )
+)
+
+products <- list(
+  "Dimer_NMQ" = list(
+    "CAS number" = "",
+    "detectWavelength" = "210",
+    "calibFactor" = 1704206,
+    "retTime" = 6.13
+    ),
+  
+  "Dimer_Cross" = list(
+    "CAS number" = "",
+    "detectWavelength" = "210",
+    "calibFactor" = NULL,
+    "retTime" = 6.9
+    ),
+  
+  "Dimer_Cum" = list(
+    "CAS number" = "",
+    "detectWavelength" = "210",
+    "calibFactor" = 1629737,
+    "retTime" = 7.75
+    )
+)
