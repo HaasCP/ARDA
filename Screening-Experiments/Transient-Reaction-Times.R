edcb1f8392db49f94cfee09654cfb586365917c2
HaasCP <chrhaas07@gmail.com>
2019-10-30 14:32:07 +0100

    Little changes and comment

--- Screening-Experiments/Transient-Reaction-Times.R
+++ Screening-Experiments/Transient-Reaction-Times.R
@@ -2,8 +2,10 @@
 # attributed to the loop numbers of the parking decks as determined by external calibration.
 ##
 
+## Am besten wird diese Liste berechnet am Ende der Kalibrierung mithilfe der Geraden.
+
 transReactTime <- list(
   # Manual input of the reaction times for each loop, necessary for transient methods
-  medium = c(0.39, 0.789, 0.976, 1.32, 1.95), # Loop 1 -> Loop 5
-  slow = c(1.95, 2.54, 2.80, 3.30, 3.9) # Loop 1 -> Loop 5
+  "medium" = c(0.39, 0.789, 0.976, 1.32, 1.95), # Loop 1 -> Loop 5
+  "slow" = c(1.95, 2.54, 2.80, 3.30, 3.9) # Loop 1 -> Loop 5
 )
