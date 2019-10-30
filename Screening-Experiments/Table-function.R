f0767c2132a7613e4d7ae3737810dd9859c4aa9e
HaasCP <chrhaas07@gmail.com>
2019-10-30 14:57:54 +0100

    fixed bug

--- Screening-Experiments/Table-function.R
+++ Screening-Experiments/Table-function.R
@@ -9,8 +9,8 @@ makeRDS <- function() {
     setwd(as.character(folders[i]))                         # sets the the wd to the ith folder of the folderlist
     resultTable <- makeResultTable(reactorVolume = 0.39)    # creates result table for the ith experiment
     resultTable$reactTime <- c(transReactTime$medium, transReactTime$slow)  # pastes the calibrated reaction times of the transient experiments
-    }
     name <- as.character(folders[i])                                      # folder name
     saveRDS(resultTable, paste0(substr(name, 1,nchar(name)-2), ".rds") )  # save the result table as rds file with the name of the sample
     setwd(wd)                                                             # reset the wd to the main folder
+  }
 }
