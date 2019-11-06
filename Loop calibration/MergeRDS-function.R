8bc2d24cd3170ed411386593dcff89ed2db9a5d2
HaasCP <chrhaas07@gmail.com>
2019-11-06 14:11:02 +0100

    Initial commit

---
+++ Loop calibration/MergeRDS-function.R
@@ -0,0 +1,20 @@
+# This function merges all rds files
+mergeRDS <- function(){
+  fileNames <- dir(path = wd)                        # character vector of all file names in the wd
+  folders <- grep("\\.D", fileNames, value = T)      # vector reduced to experimental folders
+  dataList <- list()                                 # initializes list to be filled in the following for-loop
+  
+  for (i in 1:length(folders)) {
+    setwd(as.character(folders[i]))                         # sets the the wd to the ith folder of the folderlist
+    RDS <- read_rds(grep("\\.rds", list.files(), value = T))
+    RDS <- RDS %>% 
+      group_by(reactTime) %>% 
+      mutate_at(vars(5:(length(RDS) - 4)), funs(mean)) %>% 
+      distinct(reactTime, .keep_all = TRUE)
+    dataList[[grep("\\.rds", list.files(), value = T)]] = RDS
+    setwd(wd) 
+  }
+  
+  dataTable <- bind_rows(dataList, .id = "id")            # create the dataTable from the .rds files in the dataList and adding the id of the sample
+  return(dataTable)
+}
---
+++ Loop calibration/makeRDS-function.R
@@ -0,0 +1,14 @@
+library(devtools)
+
+makeRDS <- function() {
+  fileNames <- dir(path = wd)                        # character vector of all file names in the wd
+  folders <- grep("\\.D", fileNames, value = T)      # vector reduced to experimental folders
+  
+  for (i in 1:length(folders)) {
+    setwd(as.character(folders[i]))                         # sets the the wd to the ith folder of the folderlist
+    resultTable <- makeResultTable(reactorVolume = 0.39)    # creates result table for the ith experiment
+    name <- as.character(folders[i])                                      # folder name
+    saveRDS(resultTable, paste0(substr(name, 1,nchar(name)-2), ".rds") )  # save the result table as rds file with the name of the sample
+    setwd(wd)                                                             # reset the wd to the main folder
+  }
+}
